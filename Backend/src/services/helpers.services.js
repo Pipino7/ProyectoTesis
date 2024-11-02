4587d87af66f73d1da5e618232e1eee486c352db
Felipe Perez <felipe.perez2001@alumnos.ubiobio.cl>
2024-11-01 22:17:03 -0300

    refactor: simplifica y limpia helpers.services.js en la generación de códigos únicos para fardo y prenda

--- Backend/src/services/helpers.services.js
+++ Backend/src/services/helpers.services.js
@@ -1,4 +1,3 @@
-// src/services/helpers.services.js
 import AppDataSource from '../config/ConfigDB.js';
 
 const generateCodigoFardo = async () => {
@@ -15,11 +14,10 @@ const generateCodigoFardo = async () => {
     }
   }
 
-  // Si se agotan los códigos con letra y número, generar uno aleatorio
   let codigoAleatorio;
   let existe;
   do {
-    codigoAleatorio = `Z${Math.floor(1000 + Math.random() * 9000)}`; // Ejemplo: Z0001
+    codigoAleatorio = `Z${Math.floor(1000 + Math.random() * 9000)}`; 
     existe = await fardoRepository.findOne({ where: { codigo_fardo: codigoAleatorio } });
   } while (existe);
 
@@ -28,22 +26,22 @@ const generateCodigoFardo = async () => {
 
 const generateUniqueBarcode = async () => {
   const prendaRepository = AppDataSource.getRepository('prenda');
-  const fardoRepository = AppDataSource.getRepository('fardo'); // Añadido para verificar en ambas tablas
+  const fardoRepository = AppDataSource.getRepository('fardo'); 
   let barcode;
   let existeEnPrenda;
   let existeEnFardo;
 
   do {
-    const texto = Math.floor(1000000000 + Math.random() * 9000000000).toString(); // Genera 10 dígitos
+    const texto = Math.floor(1000000000 + Math.random() * 9000000000).toString(); 
     barcode = texto;
 
-    // Verificar en la tabla de prenda
+
     existeEnPrenda = await prendaRepository.findOne({ where: { codigo_barra_prenda: barcode } });
 
-    // Verificar en la tabla de fardo
+    
     existeEnFardo = await fardoRepository.findOne({ where: { codigo_barra_fardos: barcode } });
 
-  } while (existeEnPrenda || existeEnFardo); // Repetir si el código ya existe en alguna de las tablas
+  } while (existeEnPrenda || existeEnFardo); 
 
   return barcode;
 };
