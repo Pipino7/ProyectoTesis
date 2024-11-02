5e0aa892858a2728f26625d1ac3a2c80257819f7
Felipe Perez <felipe.perez2001@alumnos.ubiobio.cl>
2024-11-01 22:09:08 -0300

    feat: añade seeder para inicializar estados de prenda en la base de datos

---
+++ Backend/src/seeders/estado.seeder.js
@@ -0,0 +1,18 @@
+import AppDataSource from '../config/ConfigDB.js';
+import Estado from '../entities/estado.js';
+
+const seedEstados = async () => {
+  const estadosIniciales = ['bodega', 'disponible', 'vendido', 'fiado'];
+  const estadoRepository = AppDataSource.getRepository(Estado);
+
+  for (let nombre_estado of estadosIniciales) {
+    const estadoExistente = await estadoRepository.findOne({ where: { nombre_estado } });
+    if (!estadoExistente) {
+      const nuevoEstado = estadoRepository.create({ nombre_estado });
+      await estadoRepository.save(nuevoEstado);
+      console.log(`Estado '${nombre_estado}' creado.`);
+    }
+  }
+};
+
+export default seedEstados;
