82c37b58c007d58cc91103a641ef4af44ac2b1f1
Felipe Perez <felipe.perez2001@alumnos.ubiobio.cl>
2024-11-01 21:58:54 -0300

    feat: añade rutas para clasificación y ajusta ruta de fardos en index.routes.js

--- Backend/src/routes/index.routes.js
+++ Backend/src/routes/index.routes.js
@@ -2,18 +2,15 @@ import express from 'express';
 import authRoutes from './auth.routes.js';  
 import userRoutes from './users.routes.js';  
 import fardoRoutes from './fardo.routes.js';
+import clasificacionRoutes from './clasificacion.routes.js';
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
-
+router.use('/clasificacion', clasificacionRoutes);
 router.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
-router.use('/fardo', (req, res, next) => {
-  console.log('Ruta /fardos llamada');
-  next();
-}, fardoRoutes);  // Log para cuando se llame a cualquier ruta bajo /fardos
-
+router.use('/fardos', fardoRoutes); 
 export default router;
