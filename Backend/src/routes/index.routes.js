cbf62bdeff905279ee48ef8ebd3fd121a6401744
Felipe Perez <felipe.perez2001@alumnos.ubiobio.cl>
2024-11-12 11:40:57 -0300

    feat(routes): agregar ruta para manejo de categorías

--- Backend/src/routes/index.routes.js
+++ Backend/src/routes/index.routes.js
@@ -3,11 +3,13 @@ import authRoutes from './auth.routes.js';
 import userRoutes from './users.routes.js';  
 import fardoRoutes from './fardo.routes.js';
 import clasificacionRoutes from './clasificacion.routes.js';
+import categoriaRoutes from './Categoria.routes.js';
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/clasificacion', clasificacionRoutes);
+router.use('/categorias', categoriaRoutes);
 router.get('/', (req, res) => {
   res.send('API funcionando');
 });
