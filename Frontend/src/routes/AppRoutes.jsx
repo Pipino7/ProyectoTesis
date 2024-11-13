73921fe97b23228c1de36e3d252cab00edb64fc7
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-11-13 14:16:10 -0300

    fix(Componente): Se Elimina el componente y se deja como Pages

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../components/Login';
 import Dashboard from '../pages/Dashboard';
-import FardoModule from '../components/FardoModule';  // Importamos el FardosModule
+import FardoModule from '../pages/FardoModule';  // Importamos el FardosModule
 import PrivateRoute from '../guards/PrivateRoutes';
 
 const AppRoutes = () => {
