3ad03cdc432b1fdac4562fc667f18305d7c33811
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2024-12-13 21:44:24 -0300

    refactor: se configuró la URL base de la API para usar variables de entorno

--- Frontend/src/services/auth.js
+++ Frontend/src/services/auth.js
@@ -1,37 +1,16 @@
-import axios from 'axios';
+import axios from './api.js';
 
-const API_URL = 'http://localhost:3200/api';  // Ajusta la URL si es necesario
-
-// Función de login
 export const login = async (email, password) => {
-  try {
-    const response = await axios.post(`${API_URL}/auth/login`, { email, password });
-    return response.data;  // Retorna los datos de la respuesta
-  } catch (error) {
-    throw error;  // Lanza el error para ser manejado por el frontend
-  }
+  const response = await axios.post(`/auth/login`, { email, password });
+  return response.data; 
 };
 
-
-
-// Función para solicitar el restablecimiento de contraseña (envío de token al correo)
 export const forgotPassword = async (email) => {
-  try {
-    const response = await axios.post(`${API_URL}/auth/forgot-password`, { email });
-    return response.data;  // Retorna el mensaje del backend
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.post(`/auth/forgot-password`, { email });
+  return response.data; 
 };
 
-
-// Función para restablecer la contraseña usando el token
 export const resetPassword = async (token, nuevaPassword) => {
-  try {
-    const response = await axios.post(`${API_URL}/auth/reset-password`, { token, nuevaPassword });
-    return response.data;  // Retorna el mensaje del backend
-  } catch (error) {
-    throw error;  // Lanza el error para ser manejado por el frontend
-  }
+  const response = await axios.post(`/auth/reset-password`, { token, nuevaPassword });
+  return response.data; 
 };
-
