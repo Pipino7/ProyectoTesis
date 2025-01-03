9ecdeb4e2ffefb21bbca2c1cd75991342c605dc4
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2025-01-03 12:55:29 -0300

    refactor: reestructurar y reagrupar entidades en index.js

---
+++ Backend/src/entities/index.js
@@ -0,0 +1,23 @@
+import Categoria from './categoria.js';
+import Cliente from './cliente.js';
+import DetalleVenta from './detalle_venta.js';
+import Estado from './estado.js';
+import Fardo from './fardo.js';
+import HistorialClasificacion from './historialclasificacion.js';
+import Prenda from './prenda.js';
+import Proveedor from './proveedor.js';
+import Usuario from './usuario.js';
+import Venta from './venta.js';
+
+export {
+  Categoria,
+  Cliente,
+  DetalleVenta,
+  Estado,
+  Fardo,
+  HistorialClasificacion,
+  Prenda,
+  Proveedor,
+  Usuario,
+  Venta,
+};
