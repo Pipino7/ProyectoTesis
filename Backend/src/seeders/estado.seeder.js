afcd796bd0fd1261e3f896b93013b239cc106797
Pipino7 <felipe.perez2001@alumnos.ubiobio.cl>
2025-01-03 12:54:43 -0300

    feat: añadir nuevos estados iniciales al seeder

--- Backend/src/seeders/estado.seeder.js
+++ Backend/src/seeders/estado.seeder.js
@@ -2,7 +2,7 @@ import AppDataSource from '../config/ConfigDB.js';
 import Estado from '../entities/estado.js';
 
 const seedEstados = async () => {
-  const estadosIniciales = ['bodega', 'disponible', 'vendido', 'fiado'];
+  const estadosIniciales = ['bodega', 'disponible', 'vendido', 'pendiente', 'efectivo', 'tarjeta', 'transferencia', 'mixto'];
   const estadoRepository = AppDataSource.getRepository(Estado);
 
   for (let nombre_estado of estadosIniciales) {
