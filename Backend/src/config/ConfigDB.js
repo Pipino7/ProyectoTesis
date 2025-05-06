bbc95ea87df5a08569e197461f05a2014794433d
Felipe Perez Diaz <136616099+Pipino7@users.noreply.github.com>
2025-05-05 20:46:06 -0400

    refactor(config): ejecuta todos los seeders desde index

--- Backend/src/config/ConfigDB.js
+++ Backend/src/config/ConfigDB.js
@@ -1,37 +1,16 @@
-import { DataSource } from 'typeorm';
-import fardo from '../entities/fardo.js';
-import prenda from '../entities/prenda.js';
-import categoria from '../entities/categoria.js';
-import venta from '../entities/venta.js';
-import detalle_venta from '../entities/detalle_venta.js';
-import proveedor from '../entities/proveedor.js';
-import usuario from '../entities/usuario.js';
-import cliente from '../entities/cliente.js';  
-import estado from '../entities/estado.js';  
-import movimiento_prenda from '../entities/movimientoPrenda.js';
-
-import { DB_URL } from './configEnv.js';
+// src/config/ConfigDB.js
+import { DataSource }     from 'typeorm';
+import { DB_URL }         from './configEnv.js';
+import * as Entities      from '../entities/index.js';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
   url: DB_URL,
-  synchronize: true,  // Usar `false` en producción para usar migraciones
+  synchronize: true,
   logging: false,
-  entities: [
-    fardo,
-    prenda,
-    categoria,
-    venta,
-    detalle_venta,
-    proveedor,
-    usuario,
-    cliente,
-    estado,
-    movimiento_prenda
-    
-  ],
-  migrations: ['src/migrations/*.js'],  
-  migrationsTableName: 'migrations_history',  
+  entities: Object.values(Entities),
+  migrations: ['src/migrations/*.js'],
+  migrationsTableName: 'migrations_history',
 });
 
 export default AppDataSource;
--- Backend/src/config/InitialSetup.js
+++ Backend/src/config/InitialSetup.js
@@ -1,15 +1,16 @@
-import createAdminUser from '../seeders/usuario.seeder.js';
-import seedEstados from '../seeders/estado.seeder.js';
+// src/config/initialSetup.js
+import * as seeders from '../seeders/index.js';
 
 const initialSetup = async () => {
-  try {
-    console.log("Iniciando configuración inicial...");
 
-    await createAdminUser();
-    await seedEstados();
-    console.log("Configuración inicial completada.");
+  try {
+
+    for (const runSeeder of Object.values(seeders)) {
+      await runSeeder();
+    }
+    console.log('Configuración inicial completada.');
   } catch (error) {
-    console.error("Error en la configuración inicial:", error);
+    console.error('Error en la configuración inicial:', error);
   }
 };
 
