38b9242d264fc6d7ef4e7fcda497eab8c3cb70cb
Felipe Perez Diaz <136616099+Pipino7@users.noreply.github.com>
2025-05-21 08:57:34 -0400

    feat: añadir rol base para usar la funcionalidad

--- Backend/src/routes/resumen.routes.js
+++ Backend/src/routes/resumen.routes.js
@@ -2,11 +2,12 @@
 import express from 'express';
 import { ResumenController } from '../controllers/resumen.controllers.js';
 import authenticationMiddleware from '../middlewares/authentication.middleware.js';
+import { allowRoles } from '../middlewares/authorization.middleware.js';
 
 const router = express.Router();
 router.use(authenticationMiddleware);
-router.get('/ultimos-dias', ResumenController.getUltimosDias);
-router.get('/por-categoria', ResumenController.getEstadisticasPorCategoria);
-router.get('/por-dia-semana', ResumenController.getPorDiaSemana);
+router.get('/ultimos-dias', allowRoles('admin'), ResumenController.getUltimosDias);
+router.get('/por-categoria', allowRoles('admin'), ResumenController.getEstadisticasPorCategoria);
+router.get('/por-dia-semana', allowRoles('admin'), ResumenController.getPorDiaSemana);
 
 export default router;
